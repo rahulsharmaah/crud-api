44fbb38cbba593a510c3120e6dba61b6d9acc7b1
Rahul Sharma <8470314+rahulsharmaah@users.noreply.github.com>
2023-09-21 20:37:17 +0530

    mongoose setup

--- app.js
+++ app.js
@@ -1,13 +1,27 @@
-const express = requirre('express')
+const express = require("express");
 const app = express();
 
 //middleware
 app.use(express.json());
 
-app.listen(3001, () =>
-{
+app.listen(3001, () => {
   console.log("Server is running on port 30001");
 });
 
-
-module.export  = app;
+const mongoose = require("mongoose");
+//configure mongoose
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/CRUD",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Connected to MongoDB");
+    }
+  }
+);
+module.export = app;
